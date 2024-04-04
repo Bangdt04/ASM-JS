c20aed35b808a7d45cdfc180f70723f7480cfe03
[do bang] <bangdtph32046@fpt.edu.vn>
2024-04-04 13:33:58 +0700

    bằng

---
+++ demo.js
@@ -0,0 +1,15 @@
+function submit() {
+    alert("Đặt vé  thành công !");
+}
+function reset() {
+    alert("Xóa thành công !");
+}
+function hello() {
+    alert("chuyển tới trang chủ !")
+}
+function datve() {
+    alert("chuyển tới trang đặt vé!");
+}
+function lienhe() {
+    alert("chuyển tới trang liên hệ!");
+}
